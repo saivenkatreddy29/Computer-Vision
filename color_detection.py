101afe56a9d0d6813f1fab40f416451dbec3f116
Sai venkat reddy sheri <saivenkatreddy2905@gmail.com>
2025-02-25 18:21:07 -0500

    3rd commit

--- color_detection.py
+++ color_detection.py
@@ -1,16 +1,34 @@
 import cv2
 import numpy as np
 from utils import get_limits
+from PIL import Image
 
-yellow = [0,255,255]
+blue = [255,0,0]
 cap = cv2.VideoCapture(0)
 
 while True:
     res,frame = cap.read()
-    cv2.imshow('frame',frame)
+    
+    # print(frame)
+    # print('\n')
+    hsv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    # print(hsv_image)
+    lowerlimit,upperlimit = get_limits(blue)
+    mask = cv2.inRange(hsv_image,lowerlimit,upperlimit)
+    print(f'mask is {mask}')
+    mask_ = Image.fromarray(mask)
+    print(f'mask image is {mask_}')
+    bbox = mask_.getbbox()
+    print(f'box is {bbox}')
+    # if bbox is not None:
+    #     x1,y1,x2,y2 = bbox
+    #     frame = cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),5)
 
-    if cv2.waitKey(40) & 0xFF == ord('q'):
-        break
+    # cv2.imshow('frame',frame)
+    # # cv2.imshow('hsv frame',hsv_image)
+    # if cv2.waitKey(40) & 0xFF == ord('q'):
+    #     break
+    # break
 
 cap.release()
 cv2.destroyAllWindows()
