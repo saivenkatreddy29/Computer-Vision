278603908641a8c44ac277be8bbcf3be6731cf4b
Sai venkat reddy sheri <saivenkatreddy2905@gmail.com>
2025-02-26 23:30:55 -0500

    4th commit

--- faceanonymizer.py
+++ faceanonymizer.py
@@ -5,10 +5,22 @@ import cv2
 import mediapipe as mp
 
 
-image_path = r"C:\Users\saive\Desktop\Github\Computer-Vision\images\face.png"
-image = cv2.imread(image_path)
+def process_image(img,face_detection):
 
-image_rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-face_detection = mp.solutions.face_detection
-out = face_detection.process(image_rgb)
-print(out)
+    H,W,_ = img.shape
+    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    out = face_detection.process(img_rgb)
+    print(out.detections)
+
+args = argparse.ArgumentParser()
+args.add_argument('--mode',default = 'image')
+args.add_argument('--filepath',default = r"C:\Users\saive\Desktop\Github\Computer-Vision\images\face.png")
+args = args.parse_args()
+
+mp_face_detection = mp.solutions.face_detection
+with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confidence = 0.5) as face_detection:
+    if args.mode in ['image']:
+
+        # read image
+        img = cv2.imread(args.filepath)
+        img = process_image(img,face_detection)
